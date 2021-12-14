64fa42971ac163d2bdb8ee9dc753e0874e780dd4
khawarabbasi051@gmail.com <khawarabbasi051@gmail.com>
2021-12-14 20:10:31 +0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import Header from './components/Header';
 import React from 'react';
 function App() {
-  const dummyData = [
-    {
-        id: 1,
-        text:"Khawar"
-    },
-    {
-        id: 2,
-        text:"Abbasi"
-    },
-]
+ 
   return (
     
     <div>
-      <h1>FYP</h1>
-      {dummyData.map((item) => (
-        <h3 key={ item.id}>{ item.text}</h3>
-      ))}
+     
+     <Header title="FYP"/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
 import PropTypes from 'prop-types'
 
-const Header = ({ title }) => {
+const Header = (props) => {
     
     
     return (
         <header>
-            <h1>{title}</h1>
-            <button onClick={onClick}>Add</button>
+            <h1>{props.title}</h1>
             
         </header>
     )
 }
-Header.defaultProps = {
-    title:"Task Tracker",
-}
-Header.propTypes = {
-    title:PropTypes.string.isRequired,
-}
+// Header.defaultProps = {
+//     title:"Task Tracker",
+// }
+// Header.propTypes = {
+//     title:PropTypes.string.isRequired,
+// }
 //CSS in Js
 // const headingStyle = {
 //     color: 'red',
